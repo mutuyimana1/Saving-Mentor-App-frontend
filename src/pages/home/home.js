6bdf1850db65972c8c7ad4741c5e5adbc65a6cba
Noella Chartine <gikundiroHappy>
2023-06-01 03:31:55 -0700

    starting saving mentor app

---
+++ src/pages/home/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="savenest-home">
+      <h1>Hello</h1>
+      <p>hbjbjb bbbbbbbbbbjb </p>
+    </div>
+  );
+}
+
+export default Home;
