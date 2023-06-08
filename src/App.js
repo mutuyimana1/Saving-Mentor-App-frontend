951facd49f8f63de131f307447172d24274a7139
umbereye26 <carineumbereye56@gmail.com>
2023-06-08 10:40:12 -0700

    my first commint on dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
-import Home from "./pages/home/home";
+import Index from "./pages/dashboard/index"
+// import Home from "./pages/home/home";
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Index/>
     </div>
   );
 }
