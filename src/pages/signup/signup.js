8d65a6ec78382ec4d0778253a88a4ee4a30d5047
nikuzwela <nikuzwelaetitia@gmail.com>
2023-06-09 14:02:19 +0200

    --amend

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -4,16 +4,41 @@ import { ToastContainer, toast,  Flip} from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css"
 
 function Signup() {
-  const name= ("SIGN UP")
-   const [inputValue, setInputValue]= useState("")
-   const handleInputChange= (e)=>{
-    setInputValue(e.target.value)
+   const name= ("SIGN UP")
+   
+   const [data, setData]= useState({FirstName:"", LastName:"", UserName:"",Email:"", Phone:"", Password:"", ConfirmPassword:"",  })
+    const handleChange= (e)=>{
+    const name= e.target.name;
+    const value= e.target.value;
+    setData({...data,[name]:value})
    }
+
    const handleSubmit =(e)=>{
     e.preventDefault();
-    if (inputValue.trim ()=== ""){
+
+    console.log(data)
+    if(data.FirstName.trim ()=== ""){
         toast.error("please fill all information", {position:toast.POSITION.BOTTOM_RIGHT,autoClose:false, theme:"colored" })
     }
+    else if(data.LastName.trim ()=== ""){
+        toast.error("please fill all information", {position:toast.POSITION.BOTTOM_RIGHT,autoClose:false, theme:"colored" })
+    }
+    else if(data.UserName.trim ()=== ""){
+        toast.error("please fill all information", {position:toast.POSITION.BOTTOM_RIGHT,autoClose:false, theme:"colored" })
+    }
+    else if(data.Email.trim ()=== ""){
+        toast.error("please fill all information", {position:toast.POSITION.BOTTOM_RIGHT,autoClose:false, theme:"colored" })
+    }
+    else if(data.Phone.trim ()=== ""){
+        toast.error("please fill all information", {position:toast.POSITION.BOTTOM_RIGHT,autoClose:false, theme:"colored" })
+    }
+    else if(data.Password.trim ()=== ""){
+        toast.error("please fill all information", {position:toast.POSITION.BOTTOM_RIGHT,autoClose:false, theme:"colored" })
+    }
+    else  if(data.ConfirmPassword.trim ()=== ""){
+        toast.error("please fill all information", {position:toast.POSITION.BOTTOM_RIGHT,autoClose:false, theme:"colored" })
+    }
+
     else {
         toast.success("You have successful sign up!", {position:toast.POSITION.TOP_RIGHT, autoClose:1000, theme:"colored"})
     }
@@ -31,16 +56,16 @@ function Signup() {
             <div className='signup-container-two'>
         <div className='signup-content-two'>
                 <h1>CREATE ACCOUNT</h1>
-                <form>
+                <form method='post'   >
          <div className='signup-name' >
-             <input type='text' placeholder='First Name' value={inputValue} />
-            <input type='text' placeholder='Last Name'/><br/></div> 
-            <input type='text' placeholder='UserName'/><br/>
-            <input type='email' placeholder='Email '/><br/>
-            <input type='phone' placeholder='Phone '/><br/>
-            <input type='password' placeholder='Password'/><br/>
-            <input type='password' placeholder='Confirm Password'/><br/>
-           <div className='check'>
+             <input type='text' placeholder='First Name' name='FirstName' onChange={handleChange} value={data.FirstName}/>
+            <input type='text' placeholder='Last Name' name='LastName' onChange={handleChange} value={data.LastName}/><br/></div> 
+            <input type='text' placeholder='UserName' name='UserName' onChange={handleChange} value={data.UserName}/><br/>
+            <input type='email' placeholder='Email ' name='Email' onChange={handleChange} value={data.Email}/><br/>
+            <input type='phone' placeholder='Phone ' name='Phone' onChange={handleChange} value={data.Phone}/><br/>
+            <input type='password' placeholder='Password' name='Password' onChange={handleChange} value={data.Password}/><br/>
+            <input type='password' placeholder='Confirm Password' name='ConfirmPassword' onChange={handleChange} value={data.ConfirmPassword}/><br/>
+           <div className='checks'>
              <input type='checkbox' name='agree' id='check' /> 
             <label>I Agree to the terms and conditions of use.</label><br/></div>
             <input type='submit'  value={name} id='signup-btn' onClick={handleSubmit}/>
