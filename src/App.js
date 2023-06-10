ff86541e0e040b19d0a6b205dc8816c5c52113e3
mutuyimana1 <amutuyimana64@gmail.com>
2023-06-10 18:03:37 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from "react";
 import Index from "./pages/dashboard/index";
-import Session from './pages/session-record/session'
-import Getincome from './pages/session-record/getincome'
-    
-import Index from '../src/pages/saving-account/index'
-// import Home from "./pages/home/home";
+import Session from "./pages/session-record/session";
+import Getincome from "./pages/session-record/getincome";
+
+import Home from "./pages/home/home";
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
-      <Index/>
-      <Session/>
-      <Getincome/>
-         <Index/>
+      <Home />
+      <Index />
+      <Session />
+      <Getincome />
     </div>
-
   );
 }
 
