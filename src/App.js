263b37fd9886c1dd28ed0ecf539164f6a24d3e39
Noella Chartine <gikundiroHappy>
2023-06-10 08:53:08 -0700

    fixed pages

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React from "react";
 
-
 import { BrowserRouter as Router } from "react-router-dom";
-import Routes  from "./componet/route/route";
-
-import Index from "./pages/dashboard/index";
-
-import Session from "./pages/session-record/session";
-import Getincome from "./pages/session-record/getincome";
-
-import Account from "../src/pages/saving-account/index";
-import Home from "./pages/home/home";
-import { BrowserRouter as Router } from "react-router-dom";
+// import Routes from "./componet/route/route";
 import Routes from "./componet/routes";
 
 // import './App.css';
 
-
 function App() {
   return (
     <div className="App">
-
-
-
       <Router>
         <Routes />
       </Router>
-
-
-
     </div>
   );
 }
