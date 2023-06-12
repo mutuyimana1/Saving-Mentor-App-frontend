5200ece25a3ed76d023a5e224ea67ed281637ab3
umbereye26 <carineumbereye56@gmail.com>
2023-06-12 07:44:58 -0700

    fix an errors

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Index from "./pages/dashboard/index";
-import Session from "./pages/session-record/session";
-import Getincome from "./pages/session-record/getincome";
+// import Session from "./pages/session-record/session";
+// import Getincome from "./pages/session-record/getincome";
 
-import Home from "./pages/home/home";
+// import Home from "./pages/home/home";
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
       <Index />
-      <Session />
-      <Getincome />
+      {/* <Session /> */}
+      {/* <Getincome /> */}
     </div>
   );
 }
